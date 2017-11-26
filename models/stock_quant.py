cdab86f352701a96e0fa7c805b4d4ffc3a53964e
Nay3li <nayeli.valencia@xmarts.com.mx>
2017-11-26 10:31:12 -0600

    Se agrego nuevo campo de costeo a el reposte Stock Valuation pivot y list

---
+++ models/stock_quant.py
@@ -0,0 +1,16 @@
+## -*- coding: utf-8 -*-
+
+from openerp import models, fields, api, _, tools
+from openerp.exceptions import UserError, RedirectWarning, ValidationError
+import xlrd
+import shutil
+import logging
+_logger = logging.getLogger(__name__)
+
+class StockQuant(models.Model):
+    _inherit ='stock.quant'
+
+    @api.one
+    def _compute_standardprice(self):
+        self.standard_price = self.product_id.standard_price
+    standard_price= fields.Float(string="Costo", compute="_compute_standardprice", store=True)
